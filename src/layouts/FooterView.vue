7d985b9e1239bff83ccc39a1136de346c65d1800
Kaylin Tran <ttknganct@gmail.com>
2025-04-05 10:47:25 +0700

    add header and footer

--- src/layouts/FooterView.vue
+++ src/layouts/FooterView.vue
@@ -1,6 +1,146 @@
 <template>
-    <div>
+    <footer class="mt-3 py-4 text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 pe-3 address-wrap">
+                    <p class="text-uppercase mb-2 fw-semibold">Địa chỉ</p>
+                    <div>
+                        <strong>Trụ sở chính:</strong>
+                        Công ty Cổ Phần Phúc Long Heritage - ĐKKD: 0316 871719 do sở KHĐT TPHCM cấp lần đầu ngày 21/05/2021
+                    </div>
+                    <div>
+                        <strong>Nhà máy:</strong>
+                        D_8D_CN Đường XE 1, Khu Công Nghiệp Mỹ Phước III, phường Mỹ Phước, thị xã Bến Cát, tỉnh Bình Dương, Việt Nam.
+                    </div>
+                    <div>
+                        <strong>Địa chỉ:</strong>
+                        Phòng 702, Tầng 7, Tòa nhà Central Plaza, số 17 Lê Duẩn, phường Bến Nghé, quận 1, Hồ Chí Minh.
+                    </div>
+                    <div>
+                        <strong>Hotline Đặt hàng:</strong>
+                        1800 6779
+                    </div>
+                    <div>
+                        <strong>Hotline Công ty:</strong>
+                        1900 2345 18 (Bấm phím 0: Lễ Tân | phím 1: CSKH)
+                    </div>
+                    <div>
+                        <strong>Email:</strong>
+                        sales@phuclong.masangroup.com, info2@phuclong.masangroup.com
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="row row-cols-2 row-cols-lg-4">
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Địa chỉ</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">Giới thiệu công ty</a>
+                                </li>
+                                <li>
+                                    <a href="#">Thư viện hình ảnh</a>
+                                </li>
+                                <li>
+                                    <a href="#">Liên hệ</a>
+                                </li>
+                                <li>
+                                    <a href="#">Hình ảnh menu</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Tuyển dụng</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">HTCK</a>
+                                </li>
+                                <li>
+                                    <a href="#">Kiosk</a>
+                                </li>
+                                <li>
+                                    <a href="#">Văn phòng</a>
+                                </li>
+                                <li>
+                                    <a href="#">Nhà máy</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Hội viên</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">Câu hỏi thường gặp (FAQ)</a>
+                                </li>
+                                <li>
+                                    <a href="#">Điều khoản và điều kiện chương trình hội viên</a>
+                                </li>
+                                <li>
+                                    <a href="#">Điều khoản & điều kiện thẻ trả trước</a>
+                                </li>
+                                <li>
+                                    <a href="#">Điều khoản & điều kiện sử dụng Thiệp Voucher</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Liên hệ</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">liên hệ</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Khuyến mãi</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">Tin khuyến mãi</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Cửa hàng</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">Danh sách cửa hàng</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="text-uppercase mb-2 fw-semibold">Điều khoản sử dụng</p>
+                            <ul class="ps-0">
+                                <li>
+                                    <a href="#">Chính sách bảo mật thông tin</a>
+                                </li>
+                                <li>
+                                    <a href="#">Chính sách đặt hàng</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <a>
+                                <img src="../assets/images/logoSaleNoti.png" alt="" class="img-fluid" width="120px">
+                            </a>
+                        </div>
+                    </div>
+                </div>
 
+            </div>
+
+        </div>
+        
+    </footer>
+    <div class="footer-bottom bg-white py-3">
+        <div class="container d-flex justify-content-between align-items-center">
+            <p class="m-0">
+                © Công ty CP Phúc Long Heritage 2025
+            </p>
+            <div class="d-flex gap-4">
+                <i class="bi bi-instagram"></i>
+                <i class="bi bi-facebook"></i>
+                <i class="bi bi-youtube"></i>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,4 +148,22 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+footer{
+    background-color: var(--primary-green);
+}
+
+a{
+    text-decoration: none;
+    color: white;
+    font-size: 15px;
+}
+
+.address-wrap>div{
+    font-size: 15px;
+}
+
+.footer-bottom{
+    color: var(--primary-green);
+}
+</style>
---
+++ src/layouts/NavBarView.vue
@@ -0,0 +1,208 @@
+<template>
+    <nav class="nav-bar mt-1">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+            <div class="container">
+                <a class="navbar-brand d-lg-none" href="#" width="60px">
+                    <img src="../assets/images/logo.png" alt="" class="img-fluid" width="60px">
+                </a>
+                <button class="navbar-toggler outline-0" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i class="bi bi-list text-success"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="container">
+                            <ul class="navbar-nav my-0 d-flex justify-content-center align-items-center gap-5">
+                                <li class="nav-item active">
+                                    <a href="" class="nav-link fw-semibold">Trang chủ</a>
+                                </li>
+                                <li class="nav-item position-relative menu-toggle">
+                                    <a class="nav-link fw-semibold">Menu</a>
+                                    <div class="child-menu position-absolute">
+                                        <p class="my-2 top-title">Bst "tea latte" và bánh banaberry mới!</p>
+                                        <div class="py-2 d-flex">
+                                            <i class="bi bi-caret-up-fill position-absolute translate-middle mt-1"></i>
+                                            <div v-for="item in menuData.child">
+                                                <p class="menu-title mb-2">{{ item.title }}</p>
+                                                <div v-if="item.child && item.child.length">
+                                                    <RecursiveMenu :child="item.child" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="nav-item position-relative san-pham-toggle">
+                                    <a href="" class="nav-link fw-semibold">Sản phẩm đóng gói</a>
+                                    <div class="child-menu-sp position-absolute py-4 d-none">
+                                        <i class="bi bi-caret-up-fill position-absolute translate-middle mt-1"></i>
+                                        <div v-for="item in spdgData.child">
+                                            <p class="menu-title mb-2">{{ item.title }}</p>
+                                            <div v-if="item.child && item.child.length">
+                                                <RecursiveMenu :child="item.child" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="nav-item position-relative vct-toggle">
+                                    <a href="" class="nav-link fw-semibold">Về chúng tôi</a>
+                                    <div class="child-menu-vct position-absolute py-4 d-none">
+                                        <i class="bi bi-caret-up-fill position-absolute translate-middle mt-1"></i>
+                                        <div class="child">
+                                            <p class="menu-title mb-2">Về phúc long</p>
+                                            <ul>
+                                                <li><a href="">Giới thiệu công ty</a></li>
+                                                <li><a href="">Thư viện hình ảnh</a></li>
+                                                <li><a href="">Liên hệ</a></li>
+                                                <li><a href="">Hình ảnh menu</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link fw-semibold">Khuyến mãi</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link fw-semibold">Hội viên</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+        </nav>
+    </nav>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from 'axios';
+import RecursiveMenu from "./RecursiveMenu.vue";
+
+const API_URL_MENU = "http://localhost:3000/menu";
+const API_URL_SPDG = "http://localhost:3000/san-pham-dong-goi";
+const spdgData = ref([]);
+const menuData = ref([]);
+
+const getMenu = async () => {
+    try {
+        const response = await axios.get(API_URL_MENU);
+        menuData.value = response.data;
+        console.log(menuData.value)
+    } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu menu:", error);
+    }
+};
+onMounted(getMenu);
+
+const getSPDG = async () => {
+    try {
+        const response = await axios.get(API_URL_SPDG);
+        spdgData.value = response.data;
+        console.log(spdgData.value)
+    } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu menu:", error);
+    }
+};
+onMounted(getSPDG);
+
+
+</script>
+
+<style scoped>
+.nav-bar {
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    background-color: var(--white);
+    margin-top: 1.5px;
+    padding-block: 10px;
+}
+
+.nav-item a {
+    text-transform: uppercase;
+    font-size: 14px;
+    color: var(--primary-gray-text);
+    text-decoration: none;
+}
+
+.nav-item a:hover {
+    color: var(--primary-green);
+}
+
+.child-menu {
+    display: none;
+    position: absolute;
+    top: 45px;
+    background-color: white;
+    left: -300%;
+    width: 500px;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+    transition: 0.3s ease;
+}
+
+.child-menu i,
+.child-menu-sp i,
+.child-menu-vct i {
+    top: -15px;
+    font-size: 25px;
+    color: rgb(252, 252, 252);
+}
+
+.child-menu-sp i {
+    left: 42%;
+}
+
+.child-menu i {
+    left: 40%;
+}
+
+.child-menu-vct i{
+    left: 33%;
+}
+.child-menu-sp,
+.child-menu-vct {
+    position: absolute;
+    top: 45px;
+    background-color: white;
+    left: -100%;
+    width: 500px;
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+}
+
+.menu-title,
+.top-title {
+    text-transform: uppercase;
+    margin-left: 50px;
+    color: var(--primary-green);
+    font-weight: 600;
+}
+
+.menu-toggle:hover .child-menu {
+    display: block !important
+}
+
+.san-pham-toggle:hover .child-menu-sp {
+    display: flex !important
+}
+
+.vct-toggle:hover .child-menu-vct {
+    display: block !important
+}
+
+.child ul li {
+    padding: 8px 0;
+}
+
+.child ul li a {
+    color: black;
+    padding: 8px 15px;
+}
+
+.child ul li a:hover {
+    background-color: var(--primary-green);
+    color: var(--white);
+}
+
+@media(max-width:990px) {
+
+    
+}
+</style>
