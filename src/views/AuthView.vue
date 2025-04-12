bb3c8ad0da2a5f46199e24fa5bca5b3e6016d378
TriTai2004 <nguyentritai24042004@gmail.com>
2025-04-12 15:05:26 +0700

    [FE] add AuthView.vue and add layouts Auth

---
+++ src/views/AuthView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="auth">
+        <RouterView/>
+    </div>
+</template>
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<style scoped>
+.auth{
+    background-color: rgb(240, 248, 255);
+    min-height: 100vh;
+    height: auto;
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+}
+</style>
